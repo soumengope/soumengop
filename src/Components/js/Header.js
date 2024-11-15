41ba3e4b9bf7790a9f02d0b0050c367851355e77
soumengope <soumengop@gmail.com>
2024-11-15 20:12:22 +0530

    updated intro section

--- src/Components/js/Header.js
+++ src/Components/js/Header.js
@@ -1,14 +1,14 @@
 import '../css/Header.css'
 const Header = ()=>{
     return(
-        <section className="main_header ">
+        <section className="main_header">
             <div className="header_logo">SOUMEN GOPE</div>
             <div className="header_socialLists">
                 <div className="socialLists">
-                <img className="github_logo" src="github.png" alt="github logo" />
+                    <img className="github_logo" src="github.png" alt="github logo" />
                 </div>
                 <div className="socialLists">
-                <img className="gmail_logo" src="gmail.png" alt="gmail logo" />
+                    <img className="gmail_logo" src="gmail.png" alt="gmail logo" />
                 </div>
                 <div className="socialLists">
                     <img className="linkdin_logo" src="linkdin.png" alt="linkdin logo" />
--- src/Components/js/Intro.js
+++ src/Components/js/Intro.js
@@ -4,21 +4,14 @@ const Intro = ()=>{
         <>
         <section className="intro_main">
             <div className="intro_left">
-                <img src="soumen.jpg" alt="my-photo" />
+                <img src="my_pic.png" alt="my-photo" />
             </div>
             <div className="intro_right">
+                <h1>Hello, my name is Soumen Gope , </h1>
+                <h3>I'm a Full-Stack Web Developer.</h3>
                 <p>
-                    I was born and raised in West Bengal, India. I have completed my Diploma in Electrical Engineering in 2019. Afterward, 
-                    I transitioned into software engineering, driven by a deep passion for full-stack web development—a field that offers endless opportunities to expand my knowledge and skills. 
-                    I am proficient in HTML, CSS, JavaScript, ReactJS, Node.js, and MongoDB, and I continually strive to reach new heights in my expertise.<br/><br/>
-
-                    Throughout my journey, I have built numerous projects using these technologies, tackling real-world challenges that have strengthened my problem-solving abilities. 
-                    My passion for coding extends beyond project work; I am eager to understand how the industry leverages programming to build reliable, production-level software solutions. 
-                    I am seeking an entry-level position to begin my career, where I can apply my dedication and adaptability in a reputable organization.<br/><br/>
-
-                    Outside of work, I enjoy photography, cooking, and exploring the world—hobbies that enhance my creativity and curiosity. 
-                    My key strengths include hard work, self-motivation, multitasking, problem-solving, effective time management, and performing well under pressure.
-                    These qualities empower me to complete projects efficiently and with attention to detail.
+                Born and raised in West Bengal, India, I completed my Diploma in Electrical Engineering in 2019. Inspired by a strong passion for technology, I transitioned into software engineering, focusing on full-stack web development. My expertise spans modern web technologies, including HTML, CSS, JavaScript, ReactJS, Node.js, and MongoDB. Committed to continuous learning, I thrive on crafting efficient, 
+                user-centric solutions while exploring the ever-evolving landscape of web development.
                 </p>
             </div>
         </section>
