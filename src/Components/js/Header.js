c5c4eb9a288b23178323256b88eec690d20eb273
soumengope <soumengop@gmail.com>
2024-11-17 15:25:33 +0530

    updated skills and hobbies styles

--- src/Components/js/Header.js
+++ src/Components/js/Header.js
@@ -6,16 +6,25 @@ const Header = ()=>{
             <div className="header_logo">SOUMEN GOPE</div>
             <div className="header_socialLists">
                 <div className="socialLists">
-                    <img className="github_logo" src="github.png" alt="github logo" />
+                    <a href="https://github.com/soumengope" target="_blank" rel="noopener noreferrer">
+                        <img className="github_logo" src="github.png" alt="github logo"/>
+                    </a>
                 </div>
                 <div className="socialLists">
-                    <img className="gmail_logo" src="gmail.png" alt="gmail logo" />
+                    <a href="mailto:soumengop@gmail.com" rel="noopener noreferrer">
+                        <img className="gmail_logo" src="gmail.png" alt="gmail logo" />
+                    </a>
                 </div>
                 <div className="socialLists">
-                    <img className="linkdin_logo" src="linkdin.png" alt="linkdin logo" />
+               
+                    <a href="https://www.linkedin.com/in/soumengop/" target="_blank" rel="noopener noreferrer">
+                        <img className="linkdin_logo" src="linkdin.png" alt="linkdin logo" />
+                    </a>
                 </div>
                 <div className="socialLists">
-                    <img className="twitter_logo" src="twitter.png" alt="twitter logo"/>
+                    <a href="https://x.com/soumen_gope" target="_blank" rel="noopener noreferrer">
+                        <img className="twitter_logo" src="twitter.png" alt="twitter logo"/>
+                    </a>
                 </div>
             </div>
         </section>
