6547d66232f6c16c509ff0c0c1dcc5abdcaf2dbe
soumengope <soumengop@gmail.com>
2025-08-20 10:15:04 +0530

    updated resume

--- src/Cv.js
+++ src/Cv.js
@@ -4,8 +4,8 @@ const Cv = ()=>{
         <>
         <h2 className="resume_header">My Resume</h2>
         <div className="img_div">
-            <img src="resume2024.jpg" alt="Soumen Gope's resume" width="796px" height="1125px" />
-            <div className="cv_download"><a href="resume2024.pdf" download>Download Resume</a></div>
+            <img src="resume.jpg" alt="Soumen Gope's resume" width="796px" height="1125px" />
+            <div className="cv_download"><a href="resume.pdf" download>Download Resume</a></div>
         </div>
         </>
     )
